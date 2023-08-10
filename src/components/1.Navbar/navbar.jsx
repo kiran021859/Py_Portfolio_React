dc0a11a827832847a238e6a4e9a8f581a9821356
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-08-10 11:10:26 +0200

    projects page

--- src/components/1.Navbar/navbar.jsx
+++ src/components/1.Navbar/navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
                   <li><a href="#homepage">Home</a></li>
                   <li><a href="#aboutpage">About</a></li>
                   <li><a href="#skillsPage">Skills</a></li>
-                  <li><a href="#portfolio">Portfolio</a></li>
+                  <li><a href="#projects">Projects</a></li>
                   <li><a href="#contact">Contact</a></li>
               </ul>
             </div>
--- src/components/5.Projects/projects.jsx
+++ src/components/5.Projects/projects.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import './projects.css'
+//import html from './pictures/html.png';
+//import css from './pictures/css.png';
+import slide from './pictures/background.png';
+import python from './pictures/python.png';
+import java from './pictures/java.png';
+import sql from './pictures/sql.png';
 
 function Projects() {
   return (
-    <section id='section' className='h-screen'>
-      <div id=' ' className=''>
+    <section id='projects' className='h-screen flex justify-center'>
+      <div id='project-container' className='max-w-[1400px] w-full py-16 px-4'>
 
-        <div id=' ' className=''>
-          <div id=' ' className=''>
-
+        <div id='card' className='w-full h-full rounded-2xl bg-center bg-cover flex items-end' style={{backgroundImage: `url(${python})`}}>
+          <div id='hover' className='w-full h-full bg-center bg-cover' style={{backgroundImage: `url(${slide})`}}>
+            
           </div>
         </div>
 
-        <div id=' ' className=''>
-          <div id=' ' className=''>
-
+        <div id='card' className='w-full h-full rounded-2xl bg-center bg-cover flex items-end' style={{backgroundImage: `url(${java})`}}>
+          <div id='hover' className='w-full h-full bg-center bg-cover' style={{backgroundImage: `url(${slide})`}}>
+          
           </div>
         </div>
 
-        <div id=' ' className=''>
-          <div id=' ' className=''>
-
+        <div id='card' className='w-full h-full rounded-2xl bg-center bg-cover flex items-end' style={{backgroundImage: `url(${sql})`}}>
+          <div id='hover' className='w-full h-full bg-center bg-cover' style={{backgroundImage: `url(${slide})`}}>
+            
           </div>
         </div>
 
