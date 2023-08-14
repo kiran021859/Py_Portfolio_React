dd5e55b040d5a9a71984ffa2b966340ea22e9dd3
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-08-14 11:07:36 +0200

    contact page done

--- src/components/6.comunicate/communication.jsx
+++ src/components/6.comunicate/communication.jsx
@@ -1,73 +1,26 @@
 import React from 'react'
 import './communication.css'
 import slide from './pictures/background.png';
+import linkedin from './pictures/linkedin-logo-24.png';
+import github from './pictures/github-logo-24.png';
+import WhatsApp from './pictures/user-regular-24.png';
 
 function Communication() {
   return (
     <section id='contact' className='h-screen flex justify-center items-center'>
-      <div id='form-container' className='w-full h-full rounded-2xl bg-center bg-cover flex justify-center items-center' style={{backgroundImage: `url(${slide})`}}>
-        <form id='form' className=''>
-          <div id='inside-form' className=''>
-            <div id='input' className='rounded-2xl'>
-
-              <div className='flex justify-center items-center'>
-                <label className='flex justify-center items-center rounded-2xl'>
-                  Name:
-                </label>
-              </div>
-
-              <div className='flex items-center'>
-                <input className='rounded-2xl'>
-                </input>
-              </div>
-
-            </div>
-
-            <div id='input' className=''>
-
-              <div className='flex justify-center items-center'>
-                <label className='flex justify-center items-center rounded-2xl'>
-                  Surname:
-                </label>
-              </div>
-
-              <div className='flex items-center'>
-                <input className='rounded-2xl'>
-                </input>
-              </div>
-            </div>
-
-            <div id='input' className=''>
-
-              <div className='flex justify-center items-center'>
-                <label className='flex justify-center items-center rounded-2xl'>
-                  Email:
-                </label>
-              </div>
-
-              <div className='flex items-center'>
-                <input className='rounded-2xl'>
-                </input>
-              </div>
-
-            </div>
-
-            <div id='input' className=''>
-
-              <div className='flex justify-center items-center'>
-                <label className='flex justify-center items-center rounded-2xl'>
-                  Text:
-                </label>
-              </div>
-
-              <div className='flex items-center'>
-                <textarea  className='rounded-2xl'>
-                </textarea>
-              </div>
-
-            </div>
-          </div>
-        </form>
+      <div className="contact-form " style={{backgroundImage: `url(${slide})`}}>
+        <form action="https://formsubmit.co/kiranisaacs12@gmail.com" method="POST">
+            <input type="name" name="name" placeholder="Your Name" required></input>
+            <input type="email" name="email" placeholder="Your Email" required></input>
+            <input type="" name="phone number" placeholder="Your Mobile Number" required></input>
+            <textarea name="message" id="" cols="35" rows="10" placeholder="How Can I Help You" required></textarea>
+        </form> 
+      </div>
+      <div id='icons-container' className=''>
+        <input type="submit" value="Send Message" id="submit" required></input>
+        <div style={{backgroundImage: `url(${linkedin})`}} className='bg-center bg-cover' id='icons'></div>
+        <div style={{backgroundImage: `url(${github})`}} className='bg-center bg-cover' id='icons'></div>
+        <div style={{backgroundImage: `url(${WhatsApp})`}} className='bg-center bg-no-repeat' id='icons'></div>
       </div>
     </section>
   )
